f1e50bb6b1f20908c47f0556bea2527d25c65e95
hollli <63290246+hollli@users.noreply.github.com>
2023-12-07 07:44:05 +0200

    Update App.java

--- src/main/java/com/example/domain/App.java
+++ src/main/java/com/example/domain/App.java
@@ -7,11 +7,7 @@ public class App {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context =
                      new ClassPathXmlApplicationContext("applicationContext.xml")) {
-
-            // Get the top-level bean
             Service topLevelService = context.getBean("firstLevelService", Service.class);
-
-            // Perform the action
             topLevelService.performAction();
         }
     }
